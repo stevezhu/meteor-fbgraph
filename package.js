2dd588fdda4ea30bd1a602ff6789882ac65161f0
Steve Zhu <st.zhu1@gmail.com>
2015-08-13 10:46:25 -0400

    Update fbgraph to v1.0.0

--- package.js
+++ package.js
@@ -1,22 +1,14 @@
-Npm.depends({'fbgraph': '0.3.0'});
-
-var path = Npm.require('path');
-var argv = process.argv.slice(2);
-var packageDirname;
-if (argv[0] === 'test-packages') {
-	packageDirname = path.basename(path.resolve(process.argv[process.argv.length - 1]));
-} else {
-	packageDirname = path.basename(process.cwd());
-}
+Npm.depends({'fbgraph': '1.0.0'});
 
 Package.describe({
+	name: "stevezhu:fbgraph",
 	summary: "Node.js module to access the Facebook graph api.",
-	version: "1.2.0",
+	version: "2.0.0",
 	git: "https://github.com/stevezhu/meteor-fbgraph.git"
 });
 
 Package.onUse(function(api) {
-	api.use('meteorhacks:npm@1.2.2');
+	api.use('meteorhacks:async@1.0.0');
 
 	api.addFiles('fbgraph.js', 'server');
 	api.export('FBGraph', 'server');
@@ -24,9 +16,7 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
 	api.use('tinytest');
-	api.use([
-		'service-configuration'
-	]);
-	api.use(packageDirname);
+	api.use('service-configuration');
+	api.use('stevezhu:fbgraph');
 	api.addFiles('tests/fbgraph_test.js');
 });
